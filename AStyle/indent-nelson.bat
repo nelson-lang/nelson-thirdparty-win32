db8af88297de3ef7459924c007b074b74a91f5de
Allan CORNET <nelson.numerical.computation@gmail.com>
2018-05-27 16:37:43 +0200

    remove AStyle

--- AStyle/indent-nelson.bat
+++
@@ -1,1 +0,0 @@
-bin\AStyle.exe --style=ansi --attach-namespaces --attach-classes --attach-inlines -Y --indent=spaces --indent-switches --indent-namespaces --xe -j --recursive  ..\..\Nelson\modules\*.cpp ..\..\Nelson\modules\*.c ..\..\Nelson\modules\*.h ..\..\Nelson\modules\*.hpp 
