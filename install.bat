6359720f82466671952c1c8466961464b4267af6
Allan CORNET <nelson.numerical.computation@gmail.com>
2018-06-02 16:26:05 +0200

    add sioclient tuned for nelson

--- install.bat
+++ install.bat
@@ -163,3 +163,6 @@ copy .\libffi\bin\*.dll ..\NelSon\bin\%PLATFORM_NELSON%\*.*
 
 rem clang-format
 copy .\clang-format\*.exe ..\NelSon\tools\clang_format\*.exe
+
+rem sioclient
+copy .\sioclient\bin\*.dll ..\NelSon\bin\%PLATFORM_NELSON%\*.*
